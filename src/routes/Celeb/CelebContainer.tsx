3705d91ad11eff2f675134063a3c7c11a336362c
Dongin Kim <miknignod@gmail.com>
2020-03-08 16:55:46 +0900

    add: Celeb component

---
+++ src/routes/Celeb/CelebContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CelebPresenter from './CelebPresenter';
+
+const CelebContainer = () => {
+  return <CelebPresenter />;
+};
+
+export default CelebContainer;
---
+++ src/routes/Celeb/CelebPresenter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Board from '../../components/Board';
+
+const CelebPresenter = () => {
+  return (
+    <>
+      <div>{/* <Nav /> */}</div>
+      <div>
+        <Board to="#"></Board>
+      </div>
+    </>
+  );
+};
+
+export default CelebPresenter;
