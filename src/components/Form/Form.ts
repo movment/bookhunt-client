dc399d978939c35009bfcd69b2c1db7e89f31690
Dongin Kim <miknignod@gmail.com>
2020-03-09 18:26:17 +0900

    update: 넓이 삭제

--- src/components/Form/Form.ts
+++ src/components/Form/Form.ts
@@ -2,13 +2,5 @@ import styled from 'styled-components';
 
 export default styled.form`
   display: flex;
-  flex-direction: column;
   align-items: center;
-  height: 500px;
-  width: 400px;
-  padding: 0 20px;
-  border: 1px solid black;
-  & > * {
-    margin-top: 10px;
-  }
 `;
