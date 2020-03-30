16b1822df688c7106fbb6068673e2adf0a3cbe48
Dongin Kim <miknignod@gmail.com>
2020-03-30 19:53:01 +0900

    update: Home Container

--- src/routes/Home/HomeContainer.tsx
+++ src/routes/Home/HomeContainer.tsx
@@ -1,12 +1,17 @@
 import React, { useState, ChangeEventHandler, FormEventHandler } from 'react';
 import HomePresenter from './HomePresenter';
 import { SEARCH_BOOKS } from './HomeQueries';
-import { useLazyQuery } from '@apollo/react-hooks';
+import { useLazyQuery, useMutation } from '@apollo/react-hooks';
 import { SearchBooks } from '../../types/api';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Redirect } from 'react-router-dom';
 import Highlight from '../../components/Highlight';
-
+import { useCookies } from 'react-cookie';
+import { USER_SIGN_IN } from '../../queries/queries.local';
+import { useApolloClient } from '@apollo/react-hooks';
 const HomeContainer: React.SFC<RouteComponentProps> = ({ history }) => {
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+
+  const client = useApolloClient();
   const [title, setTitle] = useState('');
   const [timer, setTimer] = useState(0);
   const [searchBooks, { data }] = useLazyQuery<SearchBooks>(SEARCH_BOOKS, {
@@ -14,6 +19,19 @@ const HomeContainer: React.SFC<RouteComponentProps> = ({ history }) => {
   });
   const [called, setCalled] = useState(false);
 
+  if (cookies.jwt && !localStorage.getItem('jwt')) {
+    console.log('jwt');
+    localStorage.setItem('jwt', cookies.jwt);
+    removeCookie('jwt');
+    client.writeData({
+      data: {
+        auth: {
+          __typename: 'Auth',
+          isLoggedIn: true,
+        },
+      },
+    });
+  }
   const highlight =
     title &&
     data?.SearchBooks.books?.map((book, i) => {
