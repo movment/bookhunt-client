9c3c8e22d46fd0f2913d96bea4366659b11e6ff9
Dongin Kim <miknignod@gmail.com>
2020-03-30 19:52:26 +0900

    add: Naver styled-component

---
+++ src/components/Naver/Naver.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.a`
+  display: block;
+  background-image: url('/Naver.PNG');
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+`;
